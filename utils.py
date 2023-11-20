63a73057f02beb71fb20c993d4def76237290647
jelanger <123022676+jelanger@users.noreply.github.com>
2023-11-20 15:36:15 -0700

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,42 @@
+import whisper
+from langchain.llms import OpenAI 
+from langchain.agents import initialize_agent
+from langchain.agents.agent_toolkits import ZapierToolkit
+from langchain.utilities.zapier import ZapierNLAWrapper
+import os
+
+# get from https://platform.openai.com/
+os.environ["OPENAI_API_KEY"] = "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"
+
+# get from https://nla.zapier.com/docs/authentication/ after logging in):
+os.environ["ZAPIER_NLA_API_KEY"] = "sk-ak-I9gRjtT4dCObVluv40q708SoHR"
+
+
+def email_summary(file):
+
+
+    # large language model
+    llm = OpenAI(temperature=0.0)
+
+    # Initializing zapier
+    zapier = ZapierNLAWrapper()
+    toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
+
+    # The agent used here is a "zero-shot-react-description" agent. 
+    # Zero-shot means the agent functions on the current action only — it has no memory. 
+    # It uses the ReAct framework to decide which tool to use, based solely on the tool's description.
+    agent = initialize_agent(toolkit.get_tools(), llm, agent="zero-shot-react-description", verbose=True)
+
+
+    # specify a model, here its BASE
+    model = whisper.load_model("medium.en")
+
+   
+
+    # transcribe audio file
+    result = model.transcribe(file)
+    print(result["text"])
+
+    # Send email using zapier
+    agent.run("Send an Email summarizing the following text provided below to the email address provided in the text below via gmail, don't add dashes when person is : "+result["text"])
+
